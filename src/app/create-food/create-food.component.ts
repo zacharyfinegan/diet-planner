f111e89b0f52a4de6a686098a87ea095c2fa6247
Zack <zacharyfinegan@gmail.com>
2023-02-02 16:40:10 -0500

    currentFood totalMacros implemented and presented on view; not yet able to be stored or saved

--- src/app/create-food/create-food.component.ts
+++ src/app/create-food/create-food.component.ts
@@ -12,6 +12,7 @@ export class CreateFoodComponent implements OnInit {
     macro = macroData;
     currentFood = new Array;
     headers = Object.keys(this.macro[0]);
+    totalMacrosArray: Array<number> = [0,0,0,0];
 
     constructor() { }
 
@@ -29,7 +30,31 @@ export class CreateFoodComponent implements OnInit {
     addToCurrentFood(food:Object) {
         //let x = JSON.parse(JSON.stringify(food));
         this.currentFood.push(Object.values(food));
-        console.log(this.macro[28].Name);
-        console.log(food)
+    }
+
+
+    totalMacros() {
+        let totalCalories = 0;
+        let totalFat = 0;
+        let totalCarbs = 0;
+        let totalProtein = 0;
+
+        this.currentFood.forEach(function (value) {
+            totalCalories += value[1];
+            totalFat += value[2];
+            totalCarbs += value[3];
+            totalProtein += value[4];
+        });
+
+        console.log(totalCalories, totalFat, totalCarbs, totalProtein)
+        // this.totalMacrosArray.push(totalCalories,totalFat,totalCarbs,totalProtein);
+        this.totalMacrosArray[0] = totalCalories;
+        this.totalMacrosArray[1] = totalFat;
+        this.totalMacrosArray[2] = totalCarbs;
+        this.totalMacrosArray[3] = totalProtein;
+
+        console.log(this.totalMacrosArray)
+        return this.totalMacrosArray;
+
     }
 }
