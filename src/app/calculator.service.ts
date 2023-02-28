9ee6478241c5ba9ebb9efd8eddd76b151dc2dd16
Zack <zacharyfinegan@gmail.com>
2023-02-28 16:38:22 -0500

    Added heights array to CalculatorService and started input boxes for user stats

--- src/app/calculator.service.ts
+++ src/app/calculator.service.ts
@@ -8,4 +8,7 @@ export class CalculatorService {
   constructor() { }
 
   baseURL = "https://www.calculator.net/macro-calculator.html?ctype=standard&cage=25&csex=m&cheightfeet=6&cheightinch=1&cpound=165&cheightmeter=180&ckg=65&cactivity=1.465&cgoal=m&cmop=0&cformula=m&cfatpct=20&printit=0&x=90&y=28";
+
+  heights = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84]
+  
 }
