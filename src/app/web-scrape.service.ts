14195a7407decbb307f148a1370ab8652e16d25b
Zack <zacharyfinegan@gmail.com>
2023-03-09 22:05:36 -0500

    created heroku

--- src/app/web-scrape.service.ts
+++ src/app/web-scrape.service.ts
@@ -13,7 +13,7 @@ export class WebScrapeService {
       async scrape(website: string) {
         const axiosResponse = await this.axios.request({
             method: "GET",
-            url: website,
+            url: "https://serene-scrubland-32304.herokuapp.com/" + website,
             headers: {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
             }
